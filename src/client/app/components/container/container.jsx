d20e909802023ca2ff5252388e6111ce763d3427
nevin <nevinm@qburst.com>
2017-03-03 19:01:11 +0530

    Worked out the styles importing

--- src/client/app/components/container/container.jsx
+++ src/client/app/components/container/container.jsx
@@ -1,9 +1,11 @@
 /**
  * A semantic wrapper around bootstrap's css container
  */
+import styles from './container.scss';
 
 const Container = ({children}) => (
     <div className="container">
+      <h1 className={styles.random}>Container Heading</h1>
         {children}
     </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,23 @@
 var webpack = require("webpack");
 var path = require("path");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var BUILD_DIR = path.resolve(__dirname, "src/client/public");
 var APP_DIR = path.resolve(__dirname, "src/client/app");
+
+const cssLoaderQuery = {
+  modules: true,
+  importLoaders: 2,
+  sourceMap: true,
+  localIdentName: '[local]_[hash:base64:3]',
+};
+
+const sassLoaderQuery = {
+  outputStyle: 'expanded',
+  sourceMap: true,
+};
+
+console.log(BUILD_DIR);
 
 module.exports = {
     entry: APP_DIR + "/app.jsx",
@@ -11,17 +26,28 @@ module.exports = {
         filename: "app.bundle.js"
     },
     module: {
-        loaders: [{
-            test: /\.js$/,
-            include: APP_DIR,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }, {
-            test: /\.jsx$/,
-            include: APP_DIR,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }]
+      loaders: [{
+        test: /\.js$/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }, {
+        test: /\.jsx$/,
+        include: APP_DIR,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract([
+          'css-loader?' + JSON.stringify(cssLoaderQuery),
+          'sass-loader?' + JSON.stringify(sassLoaderQuery),
+        ]),
+      },
+    ],
     },
     resolve: {
         extensions: ["", ".js", ".jsx"]
@@ -32,6 +58,7 @@ module.exports = {
     plugins: [
         new webpack.ProvidePlugin({
             "React": "react"
-        })
+        }),
+        new ExtractTextPlugin([], 'styles.css'),
     ]
 };
